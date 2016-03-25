d9bc995bb5332bdedf9499b6cb01474826acf294
henriquelima <j.henri-sl@hotmail.com>
2016-03-25 14:10:43 -0300

    Conexão Feita

---
+++ src/br/edu/ifpb/conexao/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package br.edu.ifpb.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	public Connection getConnection(){
+		try {
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/fj21", "root", "15091998");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+				
+		
+	}
+
+}
